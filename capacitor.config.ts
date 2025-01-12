5582fb2fb5defdf1f21fa1b5d9f3812a7a7a1fbc
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-12 15:39:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppNotificacionesComunidad',
+  webDir: 'www'
+};
+
+export default config;
