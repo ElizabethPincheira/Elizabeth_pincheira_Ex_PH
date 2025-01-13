deeab2fa50f698ae33f2a2e332f1a93bc5bc3dfb
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-13 17:59:10 -0300

    vinculacion formulario

--- src/app/componentes/formulario-crear-publicacion/formulario-crear-publicacion.component.ts
+++ src/app/componentes/formulario-crear-publicacion/formulario-crear-publicacion.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { IonInput, IonItem, IonButton, IonIcon, IonThumbnail } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons'
 import { cameraOutline} from 'ionicons/icons'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-crear-publicacion',
   templateUrl: './formulario-crear-publicacion.component.html',
   styleUrls: ['./formulario-crear-publicacion.component.scss'],
   standalone: true,
-  imports:[IonInput, IonItem, IonButton, IonIcon, IonThumbnail]
+  imports:[IonInput, IonItem, IonButton, IonIcon, IonThumbnail, FormsModule]
 })
 export class FormularioCrearPublicacionComponent  implements OnInit {
+
+  tituloPublicacion : string = '' ;
+  descripcionPublicacion :string = '';
 
   constructor() { 
     addIcons({cameraOutline});
