6cf4d7e6a31f8ec86b4dcda378ecb6b7a2b7fd29
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-15 15:55:02 -0300

    se agrega etiqueta ion-img

--- src/app/componentes/cardlist-ver-publicacion/cardlist-ver-publicacion.component.ts
+++ src/app/componentes/cardlist-ver-publicacion/cardlist-ver-publicacion.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IonItem, IonLabel, IonList, IonThumbnail, IonIcon,} from '@ionic/angular/standalone';
+import { IonItem, IonLabel, IonList, IonThumbnail, IonIcon, IonImg} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { trashOutline } from 'ionicons/icons';
 import { CommonModule } from '@angular/common';
@@ -11,7 +11,7 @@ import { PublicacionService } from 'src/app/servicios/publicacion.service'
   templateUrl: './cardlist-ver-publicacion.component.html',
   styleUrls: ['./cardlist-ver-publicacion.component.scss'],
   standalone: true,
-  imports: [ IonItem, IonLabel, IonList, IonThumbnail, IonIcon, CommonModule]
+  imports: [ IonItem, IonLabel, IonList, IonThumbnail, IonIcon, CommonModule, IonImg]
 })
 export class CardlistVerPublicacionComponent  implements OnInit {
 
--- src/app/componentes/formulario-crear-publicacion/formulario-crear-publicacion.component.ts
+++ src/app/componentes/formulario-crear-publicacion/formulario-crear-publicacion.component.ts
@@ -3,19 +3,25 @@ import { IonInput, IonItem, IonButton, IonIcon, IonThumbnail } from '@ionic/angu
 import { addIcons } from 'ionicons'
 import { cameraOutline} from 'ionicons/icons'
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-crear-publicacion',
   templateUrl: './formulario-crear-publicacion.component.html',
   styleUrls: ['./formulario-crear-publicacion.component.scss'],
   standalone: true,
-  imports:[IonInput, IonItem, IonButton, IonIcon, IonThumbnail, FormsModule]
+  imports:[IonInput, IonItem, IonButton, IonIcon, IonThumbnail, FormsModule, CommonModule]
 })
 export class FormularioCrearPublicacionComponent  implements OnInit {
 
   tituloPublicacion : string = '' ;
   descripcionPublicacion :string = '';
 
+  // onCreate = new EventEmitter<{titulo:string;desccripcion:string}>()
+
+// *********
+//   @Output() onCreate = new EventEmitter<{frase:string;autor:string}>()
+// *******
 
 
   constructor() { 
@@ -25,7 +31,12 @@ export class FormularioCrearPublicacionComponent  implements OnInit {
   ngOnInit() {}
 
   guardarPublicacion(){
+    // this.onCreate.emit({titulo:this.tituloPublicacion, descripcion:this.descripcionPublicacion})
+    // this.tituloPublicacion= '';
+    // this.descripcionPublicacion = '';
     console.log("se presiona boton GUARDAR")
   }
+
+  agregar(){}
 
 }
