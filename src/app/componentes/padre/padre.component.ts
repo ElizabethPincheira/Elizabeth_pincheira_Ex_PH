b7ed76907964f1ec53e660365ca6ad9a569b45ad
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-16 14:07:11 -0300

    por medio de *ngIf se logra ver un componente por página

--- src/app/componentes/padre/padre.component.ts
+++ src/app/componentes/padre/padre.component.ts
@@ -1,31 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CardlistVerPublicacionComponent } from 'src/app/componentes/cardlist-ver-publicacion/cardlist-ver-publicacion.component'
 import { FormularioCrearPublicacionComponent } from 'src/app/componentes/formulario-crear-publicacion/formulario-crear-publicacion.component'
 import { Publicacion } from 'src/app/modelo/publicacion';
 import { PublicacionService } from 'src/app/servicios/publicacion.service'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-padre',
   templateUrl: './padre.component.html',
   styleUrls: ['./padre.component.scss'],
-  standalone:true,
-  imports:[CardlistVerPublicacionComponent,FormularioCrearPublicacionComponent]
+  standalone: true,
+  imports: [CardlistVerPublicacionComponent, FormularioCrearPublicacionComponent, CommonModule]
 })
-export class PadreComponent  implements OnInit {
+export class PadreComponent implements OnInit {
 
-  constructor(private publicacionService:PublicacionService) { }
+  constructor(private publicacionService: PublicacionService) { }
 
-    listaPublicaciones:Publicacion[] = []
+  @Input() mostrarFormulario: boolean = false;
+
+  listaPublicaciones: Publicacion[] = []
 
   ngOnInit() {
     this._actualizar()
   }
 
-  private _actualizar(){
+  private _actualizar() {
     this.listaPublicaciones = this.publicacionService.getPublicacion()
   }
 
-  onCreatePublicacion($event: {titulo: string, descripcion: string}) {
+  onCreatePublicacion($event: { titulo: string, descripcion: string }) {
     const nuevapublicacion = new Publicacion($event.titulo, $event.descripcion);
     this.publicacionService.agregarPublicacion(nuevapublicacion);
     this._actualizar();
--- src/app/paginas/ver-publicaciones/ver-publicaciones.page.ts
+++ src/app/paginas/ver-publicaciones/ver-publicaciones.page.ts
@@ -4,11 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonContent } from '@ionic/angular/standalone';
 import { TituloVerPublicacionComponent } from 'src/app/componentes/titulo-ver-publicacion/titulo-ver-publicacion.component';
 import { ButtonAddComponent } from 'src/app/componentes/button-add-ver-publicacion/button-add.component'
-import { PublicacionService } from 'src/app/servicios/publicacion.service'
 import { PadreComponent } from 'src/app/componentes/padre/padre.component'
-
-
-
 
 @Component({
   selector: 'app-ver-publicaciones',
@@ -20,21 +16,11 @@ import { PadreComponent } from 'src/app/componentes/padre/padre.component'
 })
 export class VerPublicacionesPage implements OnInit {
 
-  constructor(private publicacionService:PublicacionService ) { 
-
-
-
-
-    
+  constructor( ) { 
   }
-
-
-
 
   ngOnInit() {
   }
-
-
 
 }
 
